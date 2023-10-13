5aecc24193fedae94879eb554712e2ac3b10fd19
meerajagota <meerajagota16@gmail.com>
2023-10-13 00:26:02 -0700

    Train/eval

--- train.py
+++ train.py
@@ -112,9 +112,9 @@ ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torc
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
 # poor man's data loader
-data_dir = os.path.join('data', dataset)
-train_data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
-val_data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+
+train_data = np.memmap(os.path.join('data', 'train.bin'), dtype=np.uint16, mode='r')
+val_data = np.memmap(os.path.join('data', 'val.bin'), dtype=np.uint16, mode='r')
 def get_batch(split):
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
