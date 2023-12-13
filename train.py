a52c248ae0da19c10c5ae75e1013c865b6094b1f
meerajagota <56693917+meerajagota@users.noreply.github.com>
2023-12-12 21:36:06 -0800

    Update train.py

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import numpy as np
 import torch
 import torch_neuron
 
+import torch_xla.core.xla_model as xm
 
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
@@ -55,7 +56,7 @@ min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchi
 # DDP settings
 backend = 'nccl' # 'nccl', 'gloo', etc.
 # system
-device = torch_xla.core.xla_model.xla_device()
+device = xm.xla_device()
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
 compile = True # use PyTorch 2.0 to compile the model to be faster
 # -----------------------------------------------------------------------------
@@ -238,7 +239,7 @@ while True:
                 }
                 print(f"saving checkpoint to {out_dir}")
                 torch.save(checkpoint, os.path.join(out_dir, 'ckpt.pt'))
-        torch_xla.core.xla_model.mark_step()
+        xm.mark_step()
     if iter_num == 0 and eval_only:
         break
 
