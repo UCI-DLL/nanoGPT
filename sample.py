9d2f2105d2bd12fe109dc44c9d787ebdc9cbe5df
meerajagota <56693917+meerajagota@users.noreply.github.com>
2024-02-05 01:22:43 -0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,91 +1,66 @@
-"""
-Sample from a trained model
-"""
-import os
-import pickle
-from contextlib import nullcontext
 import torch
-import tiktoken
-from model import GPTConfig, GPT
+import torch.neuron
+from transformers import AutoTokenizer
 
-# -----------------------------------------------------------------------------
-init_from = 'gpt2' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
-out_dir = 'out' # ignored if init_from is not 'resume'
-start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-num_samples = 10 # number of samples to draw
-max_new_tokens = 500 # number of tokens generated in each sample
-temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
-top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
-seed = 1337
-device = 'xla' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
-dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
-compile = False # use PyTorch 2.0 to compile the model to be faster
-exec(open('configurator.py').read()) # overrides from command line or config file
-# -----------------------------------------------------------------------------
+# Setup some example inputs
+sequence_0 = "The company HuggingFace is based in New York City"
+sequence_1 = "Apples are especially bad for your health"
+sequence_2 = "HuggingFace's headquarters are situated in Manhattan"
 
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
-torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
-device_type = 'xla' if 'cuda' in device else 'cpu' # for later use in torch.autocast
-ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
-ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased-finetuned-mrpc")
 
-# model
-if init_from == 'resume':
-    # init from a model saved in a specific directory
-    ckpt_path = os.path.join(out_dir, 'ckpt.pt')
-    checkpoint = torch.load(ckpt_path, map_location=device)
-    gptconf = GPTConfig(**checkpoint['model_args'])
-    model = GPT(gptconf)
-    state_dict = checkpoint['model']
-    unwanted_prefix = '_orig_mod.'
-    for k,v in list(state_dict.items()):
-        if k.startswith(unwanted_prefix):
-            state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
-    model.load_state_dict(state_dict)
-elif init_from.startswith('gpt2'):
-    # init from a given GPT-2 model
-    model = GPT.from_pretrained(init_from, dict(dropout=0.0))
+max_length = 128
+paraphrase = tokenizer.encode_plus(
+    sequence_0,
+    sequence_2,
+    max_length=max_length,
+    padding="max_length",
+    truncation=True,
+    return_tensors="pt",
+)
+not_paraphrase = tokenizer.encode_plus(
+    sequence_0,
+    sequence_1,
+    max_length=max_length,
+    padding="max_length",
+    truncation=True,
+    return_tensors="pt",
+)
 
-model.eval()
-model.to(device)
-if compile:
-    model = torch.compile(model) # requires PyTorch 2.0 (optional)
+# Load TorchScript back
+neuron_model = torch.jit.load("bert_neuron.pt")
 
-# look for the meta pickle in case it is available in the dataset folder
-load_meta = False
-if init_from == 'resume' and 'config' in checkpoint and 'dataset' in checkpoint['config']: # older checkpoints might not have these...
-    meta_path = os.path.join('data', checkpoint['config']['dataset'], 'meta.pkl')
-    load_meta = os.path.exists(meta_path)
-if load_meta:
-    print(f"Loading meta from {meta_path}...")
-    with open(meta_path, 'rb') as f:
-        meta = pickle.load(f)
-    # TODO want to make this more general to arbitrary encoder/decoder schemes
-    stoi, itos = meta['stoi'], meta['itos']
-    encode = lambda s: [stoi[c] for c in s]
-    decode = lambda l: ''.join([itos[i] for i in l])
-else:
-    # ok let's assume gpt-2 encodings by default
-    print("No meta.pkl found, assuming GPT-2 encodings...")
-    enc = tiktoken.get_encoding("gpt2")
-    encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
-    decode = lambda l: enc.decode(l)
+# Convert example inputs to a format that is compatible with TorchScript tracing
+example_inputs_paraphrase = (
+    paraphrase["input_ids"],
+    paraphrase["attention_mask"],
+    paraphrase["token_type_ids"],
+)
+example_inputs_not_paraphrase = (
+    not_paraphrase["input_ids"],
+    not_paraphrase["attention_mask"],
+    not_paraphrase["token_type_ids"],
+)
 
-# encode the beginning of the prompt
-if start.startswith('FILE:'):
-    with open(start[5:], 'r', encoding='utf-8') as f:
-        start = f.read()
-start_ids = encode(start)
-x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
+# Warmup
+y = neuron_model(*example_inputs_paraphrase)
 
-# run generation
-with torch.no_grad():
-    with ctx:
-        for k in range(num_samples):
-            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
-            print(decode(y[0].tolist()))
-            print('---------------')
+# Verify the TorchScript works on both example inputs
+paraphrase_classification_logits_neuron = neuron_model(*example_inputs_paraphrase)
+not_paraphrase_classification_logits_neuron = neuron_model(*example_inputs_not_paraphrase)
+print(paraphrase_classification_logits_neuron)
+print(not_paraphrase_classification_logits_neuron)
 
-print(torch.cuda.memory_summary())
+classes = ["not paraphrase", "paraphrase"]
+paraphrase_prediction = paraphrase_classification_logits_neuron[0][0].argmax().item()
+not_paraphrase_prediction = not_paraphrase_classification_logits_neuron[0][0].argmax().item()
+print(
+    'Neuron BERT says that "{}" and "{}" are {}'.format(
+        sequence_0, sequence_2, classes[paraphrase_prediction]
+    )
+)
+print(
+    'Neuron BERT says that "{}" and "{}" are {}'.format(
+        sequence_0, sequence_1, classes[not_paraphrase_prediction]
+    )
+)
