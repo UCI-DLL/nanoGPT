e2346604890dfb8fc7528ff4ae8d2a6b61aaa7d4
meerajagota <56693917+meerajagota@users.noreply.github.com>
2024-02-11 13:07:30 -0800

    Update compile.py

--- compile.py
+++ compile.py
@@ -3,15 +3,20 @@ import numpy as np
 import os
 import torch_neuron
 from torchvision import models
+from model import GPTConfig, GPT
 
-image = torch.zeros([1, 3, 224, 224], dtype=torch.float32)
 
-## Load a pretrained ResNet50 model
-model = models.resnet50(pretrained=True)
+
+## Load a pretrained nanoGPT model
+model = GPT.from_pretrained(init_from, dict(dropout=0.0))
+
+seed_text = "Once upon a time, there was a"
+input_ids = tokenizer.encode(seed_text, return_tensors="pt")
+
 
 ## Tell the model we are using it for evaluation (not training)
 model.eval()
-model_neuron = torch.neuron.trace(model, example_inputs=[image])
+model_neuron = torch.neuron.trace(model, example_inputs=input_ids)
 
 ## Export to saved model
 model_neuron.save("resnet50_neuron.pt")
