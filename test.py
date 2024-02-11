2ba61ebe023158195f386a352d0d248c74141712
meerajagota <56693917+meerajagota@users.noreply.github.com>
2024-02-11 07:11:04 -0800

    Update test.py

--- test.py
+++ test.py
@@ -44,7 +44,9 @@ image = torch.tensor(image.numpy()[np.newaxis, ...])
 model_neuron = torch.jit.load( 'resnet50_neuron.pt' )
 
 ## Predict
-results = model_neuron( image )
+results = model(image)
+
+# results = model_neuron( image )
 
 # Get the top 5 results
 top5_idx = results[0].sort()[1][-5:]
